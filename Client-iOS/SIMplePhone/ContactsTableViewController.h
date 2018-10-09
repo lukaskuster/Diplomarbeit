63a6e6c19ab5624511adb35d4241e164257a64a0
lukaskuster <mail@lukaskuster.com>
2018-10-09 23:40:37 +0200

    basic UI structure + app icon

---
+++ Client-iOS/SIMplePhone/ContactsTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  ContactsTableViewController.h
+//  SIMplePhone
+//
+//  Created by Lukas Kuster on 09.10.18.
+//  Copyright © 2018 Lukas Kuster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ContactsTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
