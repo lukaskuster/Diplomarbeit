1b93167bb4d04df0fed97c72962163c409d742a1
lukaskuster <mail@lukaskuster.com>
2018-10-09 14:09:52 +0200

    client-ios: generate xcode project

---
+++ Client-iOS/SIMplePhone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SIMplePhone
+//
+//  Created by Lukas Kuster on 09.10.18.
+//  Copyright © 2018 Lukas Kuster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Client-iOS/SIMplePhone/FirstViewController.m
@@ -0,0 +1,23 @@
+//
+//  FirstViewController.m
+//  SIMplePhone
+//
+//  Created by Lukas Kuster on 09.10.18.
+//  Copyright © 2018 Lukas Kuster. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
