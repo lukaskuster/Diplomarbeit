7dfd7d2b0c27b088b8b7430afeabb21bbb27d9cc
Quentin Wendegass <quentin.wendegass@htl-klu.at>
2018-10-11 11:05:27 +0200

    Add SMS-Test

--- Gateway/sim800_test.py
+++ Gateway/sim800_test.py
@@ -4,12 +4,14 @@ import argparse
 from sim800 import Sim800
 from functools import partial
 
+test_number = '06503333997'
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Serial connection sending AT-Commands to Sim800')
     parser.add_argument('--hangup', '-u', action='count')
     parser.add_argument('--answer', '-a', action='count')
     parser.add_argument('--dial', '-d')
+    parser.add_argument('--sms', '-s', action='count')
     parser.add_argument('--debug', action='count')
 
     # Callback function for the commands, that just prints the return value from the serial interface
@@ -40,6 +42,10 @@ if __name__ == '__main__':
         sim.hang_up_call(callback=on_serial_return)
     elif args.dial:
         sim.dial_number(args.dial, callback=on_serial_return)
+    elif args.sms:
+        sim.enter_pin('2580', callback=lambda e: print(e))
+        sim.set_sms_mode(1, callback=lambda e: print(e))
+        sim.send_sms(test_number, 'Hello from Sim800', callback=on_serial_return)
 
     # Custom AT-Commands while running
     # Only available if debug mode is not enabled
