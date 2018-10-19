4f82b08c6766cecc9c878b14cecffa4676ce5b66
Quentin Wendegass <quentin.wendegass@htl-klu.at>
2018-10-19 14:38:21 +0200

    Add deploy script and change worker filenames

---
+++ Server/deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+scp -r controller quentin@128.199.42.182:/home/quentin/app
+scp -r model quentin@128.199.42.182:/home/quentin/app
+scp -r worker quentin@128.199.42.182:/home/quentin/app
+scp basic-auth.js quentin@128.199.42.182:/home/quentin/app
+scp package.json quentin@128.199.42.182:/home/quentin/app
+scp package-lock.json quentin@128.199.42.182:/home/quentin/app
+
+
