b3c095054b25ee1e15d0e0b94baa61296a750fc7
Quentin Wendegass <quentin.wendegass@htl-klu.at>
2018-10-22 14:42:03 +0200

    Add webrtc test with signaling server

--- Gateway/backend/signaling.py
+++ Gateway/backend/signaling.py
@@ -2,73 +2,140 @@ import json
 from aiortc.contrib.signaling import object_from_string, object_to_string
 
 
-async def authenticate(socket, rule):
+async def authenticate(socket, rule, username, password):
+    """
+    Send the authentication event to the signaling server
+
+    :param socket: websocket client
+    :param rule: rule of the connection (offer, answer)
+    :param username: username of the client
+    :param password: password of the client
+    :type socket: object
+    :type rule: str
+    :type username: str
+    :type password: str
+    :return: boolean if the authentication is successful
+    """
+
+    # Authenticate request
+    # Must contain username and password
     request = {
         'event': 'authenticate',
-        'username': 'quentin@wendegass.com',
-        'password': 'test123',
+        'username': username,
+        'password': password,
         'rule': rule
     }
+
+    # Send the request to the signaling server
     await socket.send(json.dumps(request))
 
+    # Get the response and parse the json string
     data = await socket.recv()
     response = json.loads(data)
 
+    # Check if the right response arrived
     if 'event' not in response or 'authenticated' not in response:
         raise KeyError()
-
     if response['event'] != 'authenticate':
         raise ValueError
 
-    return True if response['authenticated'] else False
+    # return the outcome of the authentication
+    return response
 
 
 async def recv_answer(socket):
+    """
+    Get the ice description answer of the peer client
+
+    :param socket: websocket client
+    :type socket: object
+    :return: answer RTCSessionDescription
+    """
+
+    # Get the answer event from the server
     data = await socket.recv()
     response = json.loads(data)
 
+    # Check if the right response arrived
     if 'event' not in response or 'message' not in response:
         raise KeyError()
-
     if response['event'] != 'answer':
         raise ValueError()
 
+    # Convert the description string to a RTCSessionDescription object an return it
     return object_from_string(response['message'])
 
 
 async def recv_offer(socket):
+    """
+    Get the ice description offer of the peer client
+
+    :param socket: websocket client
+    :type socket: object
+    :return: offer RTCSessionDescription
+    """
+
+    # Get the offer event from the server
     data = await socket.recv()
     response = json.loads(data)
 
+    # Check if the right response arrived
     if 'event' not in response or 'message' not in response:
         raise KeyError()
-
     if response['event'] != 'offer':
         raise ValueError()
 
+    # Convert the description string to a RTCSessionDescription object an return it
     return object_from_string(response['message'])
 
 
 async def send_answer(socket, desc):
+    """
+    Send the answer ice description to the peer client.
+
+    :param socket: websocket client
+    :param desc: RTCSessionDescription answer
+    :type socket: object
+    :type desc: object
+    :return: nothing
+    """
+
+    # Answer event request
     request = {
         'event': 'answer',
+        # message contains the description object as a json string
         'message': object_to_string(desc)
     }
+    # Send the request to the server
     await socket.send(json.dumps(request))
 
 
 async def send_offer(socket, desc):
+    """
+    Send the offer ice description to the peer client.
+
+    :param socket: websocket client
+    :param desc: RTCSessionDescription offer
+    :type socket: object
+    :type desc: object
+    :return: nothing
+    """
+    # Wait for the start event, that indicates the peer client connected to the server
     data = await socket.recv()
     response = json.loads(data)
 
+    # Check if the right response arrived
     if 'event' not in response:
         raise KeyError()
-
     if response['event'] != 'start':
         raise ValueError()
 
+    # Offer event request
     request = {
         'event': 'offer',
+        # message contains the description object as a json string
         'message': object_to_string(desc)
     }
+
+    # Send the request to the server
     await socket.send(json.dumps(request))
--- Gateway/webrtc_test.py
+++ Gateway/webrtc_test.py
@@ -1,56 +1,64 @@
 import argparse
 import asyncio
-import logging
 import websockets
-import backend.signaling as signaling
+from backend import recv_answer, recv_offer, send_offer, send_answer, authenticate
 from aiortc import RTCPeerConnection, AudioStreamTrack
-from aiortc.contrib.signaling import add_signaling_arguments
+from aiortc.mediastreams import MediaStreamError
 
 
 async def run(pc, role):
     if role == 'answer':
         remote_track = None
 
+        # Define the track
         @pc.on('track')
         def on_track(track):
             nonlocal remote_track
             assert track.kind == 'audio'
             remote_track = track
 
+        # Ice description exchange with the signaling server
         async with websockets.connect('wss://signaling.da.digitalsubmarine.com:443') as socket:
             print('Connected')
-            await signaling.authenticate(socket, 'answer')
-            await pc.setRemoteDescription(await signaling.recv_offer(socket))
-            await pc.setLocalDescription(await pc.createAnswer())
-            await signaling.send_answer(socket, pc.localDescription)
+            auth = await authenticate(socket, 'answer', 'quentin@wendegass.com', 'test123')
+            if not auth['authenticated']:
+                raise RuntimeError(auth['error'])
 
-            print('Receiving audio...')
-            while True:
-                done, pending = await asyncio.wait([remote_track.recv()])
+            await pc.setRemoteDescription(await recv_offer(socket))
+            await pc.setLocalDescription(await pc.createAnswer())
+            await send_answer(socket, pc.localDescription)
+
+        print('Receiving audio...')
+        # Receive the audio frames until the connection closes
+        while True:
+            done, pending = await asyncio.wait([remote_track.recv()])
+            try:
                 print(list(done)[0].result())
+            except MediaStreamError:
+                print("Connection closed!")
+                break
     else:
+        # Add the track to the peer connection
         pc.addTrack(AudioStreamTrack())
 
+        # Ice description exchange with the signaling server
         async with websockets.connect('wss://signaling.da.digitalsubmarine.com:443') as socket:
+            print('Connected')
             await pc.setLocalDescription(await pc.createOffer())
-            await signaling.authenticate(socket, 'offer')
-            await signaling.send_offer(socket, pc.localDescription)
-            await pc.setRemoteDescription(await signaling.recv_answer(socket))
+            await authenticate(socket, 'offer', 'quentin@wendegass.com', 'test123')
+            await send_offer(socket, pc.localDescription)
+            await pc.setRemoteDescription(await recv_answer(socket))
 
+        # Send 10 seconds the audio stream
         print('Sending audio...')
         await asyncio.sleep(10)
+        print('Closing connection...')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Video stream from the command line')
     parser.add_argument('role', choices=['offer', 'answer'])
-    parser.add_argument('--record-to', help='Write received media to a file.'),
-    parser.add_argument('--verbose', '-v', action='count')
-    add_signaling_arguments(parser)
     args = parser.parse_args()
-
-    if args.verbose:
-        logging.basicConfig(level=logging.DEBUG)
 
     # create peer connection
     pc = RTCPeerConnection()
