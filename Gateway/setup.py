f41b92df16c14280ba42cb17db78552eda01e466
Quentin Wendegass <quentin.wendegass@htl-klu.at>
2019-01-02 19:15:51 +0100

    Adds possibilities to build the source

--- Gateway/setup.py
+++ Gateway/setup.py
@@ -1,69 +1,16 @@
-from setuptools import setup, Extension, find_packages, Command
-
-from subprocess import call
-import configparser
-from os import path, walk
-
-config = configparser.ConfigParser()
-config.read('config.ini')
-
-try:
-    from Cython.Build import cythonize
-except ImportError:
-    use_cython = False
-else:
-    use_cython = True
-
-
-def after(value, a):
-    pos_a = value.rfind(a)
-    if pos_a == -1:
-        return ""
-    adjusted_pos_a = pos_a + len(a)
-    if adjusted_pos_a >= len(value):
-        return ""
-    return value[adjusted_pos_a:]
-
-
-if use_cython:
-    extensions = cythonize('src/**/*.py', exclude='src/**/__init__.py')
-else:
-    extensions = []
-
-    dir_path = path.dirname(path.realpath(__file__))
-    for subdir, dirs, files in walk(path.join(dir_path, 'src', 'gateway')):
-        for file in files:
-            file_path = path.join(subdir, file)
-            filename, file_extension = path.splitext(file_path)
-            if file_extension == '.c':
-                extensions.append(Extension(after(filename.replace(path.sep, '.'), 'src.'), [file_path]))
-
-
-class CythonizeCommand(Command):
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        call(['./compile_cython.sh'], shell=True)
+from setuptools import find_packages
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
 
 
 setup(
-    cmdclass={
-        'cythonize': CythonizeCommand
-    },
     name='gateway',
-    # TODO: Not working with pip install, because config.ini is not present
-    # version=config['DEFAULT']['version'],
     version='0.0.3',
-    ext_modules=extensions,
+    ext_modules=cythonize([Extension("gateway.io.pcm.pcm", ["src/gateway/io/pcm/pcm.pyx"])]),
     package_dir={'': 'src'},
     packages=find_packages('src'),
-    install_requires=['aiortc', 'websockets', 'av', 'pyee', 'attr', 'asyncio',
+    install_requires=['aiortc', 'websockets', 'av', 'pyee', 'attrs', 'asyncio',
                       'pyserial', 'requests', 'aioice'],
     entry_points={
         'console_scripts': ['gatewayw=gateway.core.main:start'],
