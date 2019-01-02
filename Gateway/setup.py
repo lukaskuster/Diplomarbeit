83cb7d2c4c555895fb236164871d045f95bbb226
Quentin Wendegass <quentin.wendegass@htl-klu.at>
2019-01-02 19:20:51 +0100

    Changes version to 0.1.0

--- Gateway/setup.py
+++ Gateway/setup.py
@@ -6,7 +6,7 @@ from Cython.Build import cythonize
 
 setup(
     name='gateway',
-    version='0.0.3',
+    version='0.1.0',
     ext_modules=cythonize([Extension("gateway.io.pcm.pcm", ["src/gateway/io/pcm/pcm.pyx"])]),
     package_dir={'': 'src'},
     packages=find_packages('src'),
