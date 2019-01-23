f05acea64937431119712180ca5a83fcbdcfe417
Quentin Wendegass <quentin.wendegass@htl-klu.at>
2019-01-23 16:33:21 +0100

    Enables installation without cython

--- Gateway/setup.py
+++ Gateway/setup.py
@@ -1,15 +1,21 @@
 from setuptools import find_packages
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Build import cythonize
+
+try:
+    from Cython.Build import cythonize
+    ext = cythonize([Extension("gateway.io.pcm.pcm", ["src/gateway/io/pcm/pcm.pyx"]),
+                     Extension("gateway.utils.string_utils", ["src/gateway/utils/string_utils.pyx"])])
+except ImportError:
+    ext = [Extension('gateway.io.pcm.pcm', ['src/gateway/io/pcm/pcm.c', 'src/pcmlib/pcmlib.c'], ['src/pcmlib/']),
+           Extension('gateway.utils.string_utils', ['src/gateway/utils/string_utils.c', 'src/stringlib/stringlib.c'],
+                     ['src/stringlib/'])]
 
 
 setup(
     name='gateway',
     version='0.1.0',
-    ext_modules=cythonize([
-        Extension("gateway.io.pcm.pcm", ["src/gateway/io/pcm/pcm.pyx"]),
-        Extension("gateway.utils.string_utils", ["src/gateway/utils/string_utils.pyx"])]),
+    ext_modules=ext,
     package_dir={'': 'src'},
     packages=find_packages('src'),
     install_requires=['aiortc', 'websockets', 'av', 'pyee', 'attrs', 'asyncio',
