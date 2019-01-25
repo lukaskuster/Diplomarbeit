5edcf9f6a502e056982e0d01b0d828b614cc386a
Quentin Wendegass <quentin.wendegass@htl-klu.at>
2019-01-25 09:52:47 +0100

    Updates depoly script

--- Server/deploy.sh
+++ Server/deploy.sh
@@ -7,7 +7,6 @@ scp -r middleware quentin@128.199.42.182:/home/quentin/app
 scp package.json quentin@128.199.42.182:/home/quentin/app
 scp package-lock.json quentin@128.199.42.182:/home/quentin/app
 scp AuthKey_93W56D4882.p8 quentin@128.199.42.182:/home/quentin/app
-scp voip.pem quentin@128.199.42.182:/home/quentin/app
 scp voip.p12 quentin@128.199.42.182:/home/quentin/app
 
 
