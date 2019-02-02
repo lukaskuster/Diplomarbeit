3f3bfe96d079ff153d6019ed4cf304a140ccaf42
Quentin Wendegass <quentin.wendegass@htl-klu.at>
2019-02-03 00:07:06 +0100

    Adds pcm driver and setup program

--- Gateway/deploy.sh
+++ Gateway/deploy.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
+# Main Program
 scp dist/gateway-0.1.1.tar.gz gateway@raspberrypi.local:/home/gateway
+
+# Config Files
 scp apn-conf.json gateway@raspberrypi.local:/home/gateway
 scp config.ini gateway@raspberrypi.local:/home/gateway
 
+# PCM Driver
+scp -r src/bcm2835-pcm-driver gateway@raspberrypi.local:/home/gateway
 
 # Only for development
+# Tests
 scp -r tests gateway@raspberrypi.local:/home/gateway
+
+# BLE Setup Program
+cd src/ble
+./deploy.sh
+
