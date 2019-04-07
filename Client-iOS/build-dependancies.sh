d8f4b9b5ae8b7a6221c509104138b4ac346339a7
lukaskuster <mail@lukaskuster.com>
2019-04-07 16:49:43 +0200

    another try

--- Client-iOS/build-dependancies.sh
+++ Client-iOS/build-dependancies.sh
@@ -2,7 +2,11 @@
 # Build Dependacies
 
 echo "Run Carthage update..."
-carthage bootstrap --cache-builds --verbose --platform iOS
+while sleep 5m; do echo "=====[ $SECONDS seconds, buildroot still building... ]====="; done &
+carthage bootstrap --cache-builds --platform iOS
+kill %1
 
 echo "Run WebRTC init..."
+while sleep 5m; do echo "=====[ $SECONDS seconds, buildroot still building... ]====="; done &
 sh build-webrtc.sh
+kill %1
