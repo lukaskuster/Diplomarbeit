2667890435a0590cddc95019851c3cf73359db0b
lukaskuster <mail@lukaskuster.com>
2019-04-08 11:01:14 +0200

    try without carthage

--- Client-iOS/build-dependancies.sh
+++ Client-iOS/build-dependancies.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 # Build Dependacies
 
-carthage_loop=true
-echo "travis_fold:start:Carthage"
-echo "Run Carthage update..."
-./print-time.sh Carthage &
-carthage bootstrap --cache-builds --platform iOS
-pid=$(pgrep -f print-time)
-kill -9 $pid
-echo "travis_fold:end:Carthage"
+#echo "travis_fold:start:Carthage"
+#echo "Run Carthage update..."
+#./print-time.sh Carthage &
+#carthage bootstrap --cache-builds --platform iOS
+#pid=$(pgrep -f print-time)
+#kill -s KILL $pid
+#echo "travis_fold:end:Carthage"
 
 echo "travis_fold:start:WebRTC"
 echo "Run WebRTC init..."
 ./print-time.sh WebRTC &
 pid=$(pgrep -f print-time)
 sh build-webrtc.sh
-kill -9 $pid
+kill -s KILL $pid
 echo "travis_fold:end:WebRTC"
